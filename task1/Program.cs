25c68a788ec901034e994cad4ff43c5e034344a0
Denis <den.dvornikov.88@inbox.ru>
2023-03-05 22:51:31 +0600

    Выполнил Task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите первый элемент");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второй элемент");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a<b){
+    System.Console.WriteLine($"{b} Больше числа {a}");
+}
+else if (a>b) {
+System.Console.WriteLine($"{a} Больше числа {b}");
+}
+else{
+    System.Console.WriteLine($"Число {a} равно числу {b}");
+}
