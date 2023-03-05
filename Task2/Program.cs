363836c4f30c7283d5754ec5de8bc9588c3bbabb
Denis <den.dvornikov.88@inbox.ru>
2023-03-05 23:10:24 +0600

    Сделал задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите первый элемент");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второй элемент");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третий элемент");
+int c = Convert.ToInt32(Console.ReadLine());
+int max=a;
+if(b>max){
+    max=b;
+}
+if(c>max){
+    max=c;
+}
+
+System.Console.WriteLine($"Максимально число {max}");
