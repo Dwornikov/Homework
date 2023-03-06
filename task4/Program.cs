cdbb3999a2c5c6b3b305d01bb935e47e35ef6606
Denis <den.dvornikov.88@inbox.ru>
2023-03-06 23:09:33 +0600

    Выполнил 4 задание

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число");
+int a=Convert.ToInt32(Console.ReadLine());
+int b = 0;
+for(int i=1;i<a;i++){
+    b = i % 2;
+    if (b == 0){
+    System.Console.Write(" "+i);
+}
+}
